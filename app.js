4fc63fd68d01cc7a6a63121f612ac205e8ad846f
RafaS92 <manrvaldez..92@gmail.com>
2020-10-03 12:11:57 -0500

    creating servers

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const EventEmitter = require("events");
+// const EventEmitter = require("events");
 
-const Logger = require("./logger");
-const logger = new Logger();
+// const Logger = require("./logger");
+// const logger = new Logger();
 
-logger.on("messageLogged", (arg) => {
-  console.log("Listener called", arg);
+// logger.on("messageLogged", (arg) => {
+//   console.log("Listener called", arg);
+// });
+
+// logger.log("message");
+
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
-logger.log("message");
+server.listen(3000);
+
+console.log("Listening on port 3000...");
